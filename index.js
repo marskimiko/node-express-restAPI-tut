c8af3efc33abd22aca2fd4ff3873dcef79a16ab4
Marisa Saunders <marisaksaunders@gmail.com>
2024-02-27 12:58:19 -0500

    fix joi validate code

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 const courses = [
   { id: 1, name: 'course1'},
   { id: 2, name: 'course2'},
   { id: 3, name: 'course3'},
-]
+];
 
 app.get('/', (req, res) => {
   res.send('Hello World!!!!');
@@ -15,8 +18,34 @@ app.get('/api/courses', (req, res) => {
   res.send(courses);
 });
 
+app.post('/api/courses', (req, res) => {
+  const schema = Joi.object(
+    {
+      name: Joi.string().min(3).required()
+    }
+  );
+
+  const result = schema.validate(req.body);
+  console.log(result)
+
+  if (!req.body.name || req.body.name.length < 3) {
+    // 400 bad request
+    res.status(400).send('Name is required and should be minimum 3 characters');
+    return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+  courses.push(course);
+  res.send(course);
+});
+
 app.get('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('The course with the given ID was not found.');
+  res.send(course);
 });
 
 // app.get('/api/posts/:year/:month', (req, res) => {
