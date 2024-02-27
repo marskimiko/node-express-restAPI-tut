2fd88e4ddf1b4ef75ae05db447f43ec554270dee
Marisa Saunders <marisaksaunders@gmail.com>
2024-02-27 13:41:27 -0500

    finish delete

--- index.js
+++ index.js
@@ -20,10 +20,7 @@ app.get('/api/courses', (req, res) => {
 
 app.post('/api/courses', (req, res) => {
   const { error } = validateCourse(req.body);
-  if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(error.details[0].message);
 
   const course = {
     id: courses.length + 1,
@@ -35,14 +32,11 @@ app.post('/api/courses', (req, res) => {
 
 app.put('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send('The course with the given ID was not found.');
-
+  if (!course) return res.status(404).send('The course with the given ID was not found.');
+  
   // const result = validateCourse(req.body);
   const { error } = validateCourse(req.body); // result.error
-  if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(error.details[0].message);
 
   course.name = req.body.name;
   res.send(course);
@@ -59,14 +53,22 @@ function validateCourse(course) {
 
 app.get('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send('The course with the given ID was not found.');
+  if (!course) return res.status(404).send('The course with the given ID was not found.');
   res.send(course);
 });
 
-// app.get('/api/posts/:year/:month', (req, res) => {
-//   res.send(req.params);
-// });
+app.delete('/api/courses/:id', (req, res) => {
+  // Lookup and error handle
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send('The course with the given ID was not found.');
 
-// PORT 
+  // Delete
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  // Return response to client
+  res.send(course);
+})
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
