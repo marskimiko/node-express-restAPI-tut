ed43ae2814f2b1bce34d53543623819b636a9ad0
Marisa Saunders <marisaksaunders@gmail.com>
2024-02-27 12:30:04 -0500

    add courses array

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+  { id: 1, name: 'course1'},
+  { id: 2, name: 'course2'},
+  { id: 3, name: 'course3'},
+]
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World!!!!');
 });
 
 app.get('/api/courses', (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
 });
 
-app.listen(3000, () => console.log('Listening on port 3000...'));
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+});
+
+// app.get('/api/posts/:year/:month', (req, res) => {
+//   res.send(req.params);
+// });
+
+// PORT 
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
